1d8f7b55013e1e0b4b2aa1981249a355fc6bb41a
Pavel Khareuka <paul.horevko@gmail.com>
2019-03-24 20:49:01 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function row(n, i) {
+    for (let j = 0; j < 9; j++) {
+      if (matrix[i][j] == n) 
+        return false;
+    }
+    return true;
+  }
+  function col(n, j) {
+    for (let i = 0; i < 9; i++) {
+      if (matrix[i][j] == n) 
+        return false;
+    }
+    return true;
+  }
+  function box(n, i, j) {
+    let rBox = i - i % 3;
+    let cBox = j - j % 3;
+    for (let i = rBox; i < rBox + 3; i++) {
+      for (let j = cBox; j < cBox + 3; j++) {
+        if (matrix[i][j] == n) 
+          return false;
+      }
+    }
+    return true;
+  }
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (matrix[i][j] === 0) {
+        for (let n = 1; n < 10; n++) {
+          if ( row(n, i) && col(n, j) && box(n, i, j) ) {
+            matrix[i][j] = n;
+            if (solveSudoku(matrix)) {
+              return matrix;
+            } else {
+              matrix[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    } 
+  }
+  return true;
 }
+
